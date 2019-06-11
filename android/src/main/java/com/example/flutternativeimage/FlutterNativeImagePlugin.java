7ed216836f5fe7049e168734ab23e0d3e8160ae3
Nic Ford <nicford@nicford.co.uk>
2019-06-11 18:57:03 +0100

    Do the right import

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -1,6 +1,6 @@
 package com.example.flutternativeimage;
 
-import android.app.Context;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ExifInterface;
